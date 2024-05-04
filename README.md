8bfa0d3b31c5ef1fa92059bda663410db52ade83
Mehmet Fatih DANACI <mehmetfatihdanaci@gmail.com>
2024-05-04 16:45:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+##
+
+- react-router-dom
+- react-select
+- tailwind
+- framer-motion
+
+##
+
+- Imagin Studio: https://cdn.imagin.studio/getimage
+- API:https://rapidapi.com/apininjas/api/cars-by-api-ninjas
+# Car-Rental
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import CustomFilter from "../components/CustomFilter";
+import Hero from "../components/Hero";
+import SearchBar from "../components/SearchBar";
+import Card from "../components/Card";
+import { fuels, years } from "../constants";
+import { useSearchParams } from "react-router-dom";
+import { fetchCars } from "../utils";
+import { CarType } from "../types";
+import ShowMore from "../components/ShowMore";
+
+const MainPage = () => {
+  const [cars, setCars] = useState<CarType[]>([]);
+
+  const [params, setParams] = useSearchParams();
+
+  useEffect(() => {
+    const paramsObj = Object.fromEntries(params.entries());
+
+    fetchCars(paramsObj).then((res: CarType[]) => setCars(res));
+  }, [params]);
+
+  return (
+    <div>
+      <Hero />
+
+      <div id="catalogue" className="mt-12 padding-x padding-y max-width">
+        <div className="home__text-container ">
+          <h1 className="text-4xl font-extrabold">Araba Kataloğu</h1>
+          <p>Beğenebileceğin arabaları keşfet</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="Yakıt Tipi" options={fuels} />
+            <CustomFilter title="Yıl" options={years} />
+          </div>
+        </div>
+        {!cars || cars.length < 1 ? (
+          <div className="home__error-container">
+            <h2>Üzgünüz Herhangi Bir Sonuç Bulunumadı</h2>
+          </div>
+        ) : (
+          <section>
+            <div className="home__cars-wrapper">
+              {cars.map((car, i) => (
+                <Card car={car} key={i} />
+              ))}
+            </div>
+          </section>
+        )}
+        <ShowMore />
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
